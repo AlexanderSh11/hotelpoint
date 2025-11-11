a2afb35428c49d99a1a53e77d941a9fe3870fd59
Александр Шаринский <alexandersh@tpu.ru>
2025-11-11 19:15:03 +0700

    Страница с детальной информацией о номере

--- hotelpoint_project/rooms/views.py
+++ hotelpoint_project/rooms/views.py
@@ -1,7 +1,13 @@
 from django.views.generic import ListView, DetailView
 from .models import Room
 
+
 class RoomListView(ListView):
     model = Room
     context_object_name = 'rooms'
     queryset = Room.objects.select_related('category').all()
+
+
+class RoomDetailView(DetailView):
+    model = Room
+    context_object_name = 'room'
