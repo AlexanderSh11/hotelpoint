f6c25201101ebff89c111cae4978f19dbeb3a2a3
Александр <shsashao811@gmail.com>
2025-11-12 15:31:47 +0700

    Изменение модели Client

--- hotelpoint_project/clients/models.py
+++ hotelpoint_project/clients/models.py
@@ -3,10 +3,9 @@ from rooms.models import BaseModel
 
 
 class Client(BaseModel):
-    name = models.CharField(
-        max_length=150,
-        verbose_name='ФИО клиента'
-    )
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    middle_name = models.CharField(max_length=50, blank=True, null=True, verbose_name='Отчество')
     phone = models.CharField(
         max_length=20,
         verbose_name='Телефон'
@@ -17,10 +16,17 @@ class Client(BaseModel):
         verbose_name='Электронная почта'
     )
 
+    @property
+    def name(self):
+        """Возвращает полное имя"""
+        if self.middle_name:
+            return f"{self.last_name} {self.first_name} {self.middle_name}"
+        return f"{self.last_name} {self.first_name}"
+
     class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'Клиенты'
-        ordering = ('name',)
+        ordering = ('last_name',)
 
     def __str__(self):
         return f'{self.name} ({self.phone})'
